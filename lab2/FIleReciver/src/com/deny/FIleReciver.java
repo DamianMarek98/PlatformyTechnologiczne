12242bb09d2a8722a5b44419156b5dd9a233da6d
Damian Marek <43189598+DamianMarek98@users.noreply.github.com>
2019-03-13 19:54:18 +0100

    Add files via upload

--- lab2/FIleReciver/src/com/deny/FIleReciver.java
+++ lab2/FIleReciver/src/com/deny/FIleReciver.java
@@ -1,23 +1,74 @@
 package com.deny;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static java.lang.System.out;
 import static java.lang.System.in;
 
 public class FIleReciver {
-    private static int port = 1000;
+    private static int port = 1;
 
     public static void reciveFile(Socket client) throws IOException{
         String name;
         Long size;
 
-        byte[] buffer = new byte[4096];
-        int readSize;
+        DataInputStream dis = new DataInputStream(client.getInputStream());
+        name = dis.readUTF();
+        size = dis.readLong();
 
-        while ((readSize = in.read(buffer)) != -1) {
-            out.write(buffer, 0, readSize);
+
+        File file = new File(".", name);
+        out.println("Downloading "+ name + "size: " + size +" bytes");
+        //downloading starts here
+
+        FileOutputStream fos = new FileOutputStream(file);
+        try{
+            int fullRead = 0, read=0;
+            byte[] buffer = new byte[512];
+
+            while(fullRead<size){
+                read = dis.read(buffer);
+
+                if(read != -1){
+                    fullRead += read;
+                } else {
+                  throw new IOException("Couldn't read whole file!");
+                }
+                fos.write(buffer, 0, read);
+            }
+            out.println("Downloading file: "+ name+" finished!");
+            fos.close();
+        }catch (IOException e){
+            out.println("Error");
+            file.delete();
         }
     }
+
+    public static void main(String[] args) throws IOException{
+        port=1;
+
+        ExecutorService executor = Executors.newFixedThreadPool(16);
+
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            while(true){
+                final Socket socket = serverSocket.accept();
+                executor.submit(()->{
+                    try {
+                        FIleReciver.reciveFile(socket);
+                    }catch (IOException e){
+                        out.println("Problem while downloading "+ e.getMessage());
+                    }
+                });
+            }
+        }catch (IOException e){
+            out.println("Problem while accepting new clients.");
+            System.exit(1);
+        }
+    }
+
+
 }
